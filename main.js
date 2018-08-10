88c0277359e95d9f15d1f373200a83b1156710b0
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-08-10 19:25:21 +0100

    Added devtools method.

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function createWindow () {
     win = new BrowserWindow({width: 800, height: 600})
 
     win.loadFile('index.html')
+    
+    // This code below open the Developer Tool because Electron uses a browser instance !.
+    win.webContents.openDevTools()
+    
 }
 
 app.on('ready', createWindow)
